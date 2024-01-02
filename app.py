34a0ba3ece07f02a8cfc9fba9c09645c8905cbcb
andrewboylan <andreweboylan@gmail.com>
2024-01-01 19:02:12 -0500

    checkpoint before web socket

--- app.py
+++ app.py
@@ -29,8 +29,26 @@ def upload_file():
 
     try:
         file.save(file_path)
-        return jsonify({"message": "File successfully uploaded", "filename": filename}), 200
 
+        with open(file_path, 'rb') as file:
+            binary_data = file.read()
+
+        # Using hexadecimal ensures that the data is
+        # represented using only printable characters.
+            
+        # Get hex data for first 20 bytes
+        hex_data_20 = binary_data[:5].hex()
+
+        # Flip the bits
+        flipped_data = bytes(~byte & 0xFF for byte in binary_data)
+
+        # Get hex of flipped data for first 20 bytes
+        hex_flipped_data_20 = flipped_data[:5].hex()
+
+        return jsonify({"message": "File successfully uploaded", 
+                        "filename": filename, 
+                        "data_head": hex_data_20,
+                        "data_head_flipped": hex_flipped_data_20}), 200
     except Exception as e:
         # If an error occurs, delete the file
         if os.path.exists(file_path):
